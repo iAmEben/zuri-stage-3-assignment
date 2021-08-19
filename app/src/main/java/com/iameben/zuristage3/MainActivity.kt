20a4389214379f3dfcd7c7bb5f3c00c3f3946dc9
Ebenezer Justice <justiceebenezer4@gmail.com>
2021-08-19 09:25:57 +0100

    init

--- app/src/main/java/com/iameben/zuristage3/MainActivity.kt
+++ app/src/main/java/com/iameben/zuristage3/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.iameben.zuristage3
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -9,6 +11,7 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import androidx.core.widget.addTextChangedListener
 import com.iameben.zuristage3.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -22,12 +25,30 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
+        // the following lines of codes gets text from an editText and
+        // passes that input text to a textView
         binding.okBtn.setOnClickListener {
-            val msg = binding.EdttxtTil?.editText.toString()
+            val msg = binding.inputTiE?.text.toString()
             binding.displayTV.text = msg
 
-
         }
+
+        binding?.okBtn?.isEnabled = false
+        binding.inputTiE.addTextChangedListener(object: TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+               binding?.okBtn?.isEnabled = p0.toString().trim{ it <= ' '}.isNotEmpty()
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+
+            }
+
+        })
 
     }
 }
